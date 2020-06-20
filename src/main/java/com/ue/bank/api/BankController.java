6e7e3fe9e9bcbfc148e80479fd4e98861b463d71
catch441 <lukas.heubach@gmx.de>
2020-06-20 10:56:28 +0200

    [UE-59] Refactoring completed

--- src/main/java/com/ue/bank/api/BankController.java
+++ src/main/java/com/ue/bank/api/BankController.java
@@ -1,21 +1,15 @@
 package com.ue.bank.api;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import com.ue.bank.impl.BankAccountImpl;
+import com.ue.bank.impl.BankAccountSavefileHandler;
 import com.ue.exceptions.GeneralEconomyException;
 import com.ue.exceptions.GeneralEconomyExceptionMessageEnum;
-import com.ue.ultimate_economy.UltimateEconomy;
-
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 
 public class BankController {
 
 	private static List<BankAccount> accounts = new ArrayList<>();
-	private static File bankFile;
 
 	/**
 	 * Creats a new bank account with a given start amount.
@@ -25,10 +19,8 @@ public class BankController {
 	 */
 	public static BankAccount createBankAccount(double startAmount) {
 		BankAccount account = new BankAccountImpl(startAmount);
-		accounts.add(account);
-		FileConfiguration config = YamlConfiguration.loadConfiguration(bankFile);
-		config.set("Ibans", getIbanList());
-		saveConfig(config);
+		getBankAccounts().add(account);
+		BankAccountSavefileHandler.saveIbanList(getIbanList());
 		return account;
 	}
 
@@ -44,10 +36,8 @@ public class BankController {
 		if (getIbanList().contains(externalIban)) {
 			throw GeneralEconomyException.getException(GeneralEconomyExceptionMessageEnum.ALREADY_EXISTS, externalIban);
 		}
-		accounts.add(new BankAccountImpl(startAmount, externalIban));
-		FileConfiguration config = YamlConfiguration.loadConfiguration(bankFile);
-		config.set("Ibans", getIbanList());
-		saveConfig(config);
+		getBankAccounts().add(new BankAccountImpl(startAmount, externalIban));
+		BankAccountSavefileHandler.saveIbanList(getIbanList());
 	}
 
 	/**
@@ -56,31 +46,18 @@ public class BankController {
 	 * @param account
 	 */
 	public static void deleteBankAccount(BankAccount account) {
-		accounts.remove(account);
-		FileConfiguration config = YamlConfiguration.loadConfiguration(bankFile);
-		config.set("Ibans", getIbanList());
-		config.set(account.getIban(), null);
-		saveConfig(config);
+		getBankAccounts().remove(account);
+		BankAccountSavefileHandler.saveIbanList(getIbanList());
+		BankAccountSavefileHandler.deleteAccount(account.getIban());
 	}
 
 	/**
 	 * Loads all bank accounts.
 	 */
 	public static void loadBankAccounts() {
-		bankFile = new File(UltimateEconomy.getInstance.getDataFolder(), "BankAccounts.yml");
-		FileConfiguration config = YamlConfiguration.loadConfiguration(bankFile);
-		if (!bankFile.exists()) {
-			try {
-				bankFile.createNewFile();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			config.set("Ibans", new ArrayList<>());
-			saveConfig(config);
-		} else {
-			for (String iban : config.getStringList("Ibans")) {
-				accounts.add(new BankAccountImpl(iban));
-			}
+		BankAccountSavefileHandler.setupSavefile();
+		for (String iban : BankAccountSavefileHandler.loadIbanList()) {
+			getBankAccounts().add(new BankAccountImpl(iban));
 		}
 	}
 
@@ -110,15 +87,6 @@ public class BankController {
 	}
 
 	/**
-	 * Returns the savefile of the bank accounts.
-	 * 
-	 * @return bankfile
-	 */
-	public static File getSavefile() {
-		return bankFile;
-	}
-
-	/**
 	 * Returns a list of all ibans.
 	 * 
 	 * @return list of strings
@@ -129,13 +97,5 @@ public class BankController {
 			ibans.add(account.getIban());
 		}
 		return ibans;
-	}
-
-	private static void saveConfig(FileConfiguration config) {
-		try {
-			config.save(bankFile);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 }
--- src/main/java/com/ue/bank/impl/BankAccountImpl.java
+++ src/main/java/com/ue/bank/impl/BankAccountImpl.java
@@ -1,116 +1,102 @@
 package com.ue.bank.impl;
 
-import java.io.IOException;
 import java.util.UUID;
 
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
-
 import com.ue.bank.api.BankAccount;
-import com.ue.bank.api.BankController;
 import com.ue.exceptions.GeneralEconomyException;
 import com.ue.exceptions.GeneralEconomyExceptionMessageEnum;
 
 public class BankAccountImpl implements BankAccount {
 
-    private double amount;
-    private final String iban;
-    
-    /**
-     * Constructor for creating a new bank account.
-     * 
-     * @param startAmount
-     */
-    public BankAccountImpl(double startAmount) {
-	iban = UUID.randomUUID().toString();
-	setAmount(startAmount);
-	saveAmount(startAmount);
-    }
-    
-    /**
-     * Constructor for creating a account with a external iban.
-     * @param startAmount
-     * @param externalIban
-     */
-    public BankAccountImpl(double startAmount, String externalIban) {
-	iban = externalIban;
-	setAmount(startAmount);
-	saveAmount(startAmount);
-    }
-    
-    /**
-     * Constructor for loading a existing bank account.
-     * @param iban
-     */
-    public BankAccountImpl(String iban) {
-	this.iban = iban;
-	loadAmount();
-    }
+	private double amount;
+	private final String iban;
+	private BankAccountSavefileHandler savefileHandler;
 
-    @Override
-    public void decreaseAmount(double amount) throws GeneralEconomyException {
-	checkForPositiveAmount(amount);
-	if(this.amount < amount) {
-	    throw GeneralEconomyException.getException(GeneralEconomyExceptionMessageEnum.NOT_ENOUGH_MONEY);
-	} else {
-	    this.amount -= amount;
-	    saveAmount(this.amount);
+	/**
+	 * Constructor for creating a new bank account.
+	 * 
+	 * @param startAmount
+	 */
+	public BankAccountImpl(double startAmount) {
+		iban = UUID.randomUUID().toString();
+		savefileHandler = new BankAccountSavefileHandler(getIban());
+		setAmount(startAmount);
+		getSavefileHandler().saveAmount(getAmount());
 	}
-    }
 
-    @Override
-    public void increaseAmount(double amount) throws GeneralEconomyException {
-	checkForPositiveAmount(amount);
-	this.amount += amount;
-	saveAmount(this.amount);
-    }
-
-    private void checkForPositiveAmount(double amount) throws GeneralEconomyException {
-	if(amount < 0) {
-	    throw GeneralEconomyException.getException(GeneralEconomyExceptionMessageEnum.INVALID_PARAMETER, amount);
+	/**
+	 * Constructor for creating a account with a external iban.
+	 * 
+	 * @param startAmount
+	 * @param externalIban
+	 */
+	public BankAccountImpl(double startAmount, String externalIban) {
+		iban = externalIban;
+		savefileHandler = new BankAccountSavefileHandler(getIban());
+		setAmount(startAmount);
+		getSavefileHandler().saveAmount(getAmount());
 	}
-    }
-    
-    @Override
-    public boolean hasAmount(double amount) throws GeneralEconomyException {
-	checkForPositiveAmount(amount);
-	if(this.amount < amount) {
-	    return false;
-	} else {
-	    return true;
+
+	/**
+	 * Constructor for loading an existing bank account.
+	 * 
+	 * @param iban
+	 */
+	public BankAccountImpl(String iban) {
+		this.iban = iban;
+		savefileHandler = new BankAccountSavefileHandler(getIban());
+		setAmount(getSavefileHandler().loadAmount());
 	}
-    }
 
-    @Override
-    public double getAmount() {
-	return amount;
-    }
-    
-    private void setAmount(double amount) {
-	this.amount = amount;
-    }
-    
-    private void loadAmount() {
-	FileConfiguration config = YamlConfiguration.loadConfiguration(BankController.getSavefile());
-	setAmount(config.getDouble(iban + ".amount"));
-    }
-    
-    private void saveAmount(double amount) {
-	FileConfiguration config = YamlConfiguration.loadConfiguration(BankController.getSavefile());
-	config.set(iban + ".amount", amount);
-	saveConfig(config);
-    }
-
-    @Override
-    public String getIban() {
-	return iban;
-    }
-
-    private static void saveConfig(FileConfiguration config) {
-	try {
-	    config.save(BankController.getSavefile());
-	} catch (IOException e) {
-	    e.printStackTrace();
+	@Override
+	public void decreaseAmount(double amount) throws GeneralEconomyException {
+		checkForPositiveAmount(amount);
+		if (getAmount() < amount) {
+			throw GeneralEconomyException.getException(GeneralEconomyExceptionMessageEnum.NOT_ENOUGH_MONEY);
+		} else {
+			setAmount(getAmount() - amount);
+			getSavefileHandler().saveAmount(getAmount());
+		}
 	}
-    }
+
+	@Override
+	public void increaseAmount(double amount) throws GeneralEconomyException {
+		checkForPositiveAmount(amount);
+		setAmount(getAmount() + amount);
+		getSavefileHandler().saveAmount(getAmount());
+	}
+
+	private void checkForPositiveAmount(double amount) throws GeneralEconomyException {
+		if (amount < 0) {
+			throw GeneralEconomyException.getException(GeneralEconomyExceptionMessageEnum.INVALID_PARAMETER, amount);
+		}
+	}
+
+	@Override
+	public boolean hasAmount(double amount) throws GeneralEconomyException {
+		checkForPositiveAmount(amount);
+		if (getAmount() < amount) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	@Override
+	public double getAmount() {
+		return amount;
+	}
+
+	private void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	@Override
+	public String getIban() {
+		return iban;
+	}
+
+	private BankAccountSavefileHandler getSavefileHandler() {
+		return savefileHandler;
+	}
 }
