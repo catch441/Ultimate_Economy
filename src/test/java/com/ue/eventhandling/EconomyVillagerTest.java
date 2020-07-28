f7596b7efc513b1bc57458a82636bda6d2a56eef
catch441 <lukas.heubach@gmx.de>
2020-07-28 21:16:27 +0200

    [UE-57] added unit tests

---
+++ src/test/java/com/ue/eventhandling/EconomyVillagerTest.java
@@ -0,0 +1,43 @@
+package com.ue.eventhandling;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class EconomyVillagerTest {
+
+	@Test
+	public void getEnumAdminshop() {
+		assertEquals(EconomyVillager.ADMINSHOP, EconomyVillager.getEnum("adminshop"));
+	}
+	
+	@Test
+	public void getEnumPlayershop() {
+		assertEquals(EconomyVillager.PLAYERSHOP, EconomyVillager.getEnum("playershop"));
+	}
+	
+	@Test
+	public void getEnumRentshop() {
+		assertEquals(EconomyVillager.PLAYERSHOP_RENTABLE, EconomyVillager.getEnum("playershop_Rentable"));
+	}
+	
+	@Test
+	public void getEnumPlotsale() {
+		assertEquals(EconomyVillager.PLOTSALE, EconomyVillager.getEnum("plotSale"));
+	}
+	
+	@Test
+	public void getEnumTownmanager() {
+		assertEquals(EconomyVillager.TOWNMANAGER, EconomyVillager.getEnum("townManager"));
+	}
+	
+	@Test
+	public void getEnumJobcenter() {
+		assertEquals(EconomyVillager.JOBCENTER, EconomyVillager.getEnum("jobcenter"));
+	}
+	
+	@Test
+	public void getEnumUndefined() {
+		assertEquals(EconomyVillager.UNDEFINED, EconomyVillager.getEnum("kthschnll"));
+	}
+}
